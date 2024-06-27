743733d65e9fb3f85d949f3ebf91c7d742ea933a
Арсений <115050782+ArPaSokolov@users.noreply.github.com>
2024-06-28 00:25:33 +0200

    Create create_data.py
    generates json with data{"vehicleId", "vehicleName", "receiverEmail", "receiverPhone", "stops"}

---
+++ Summer Practice/create_data.py
@@ -0,0 +1,83 @@
+import json
+import random
+from datetime import timedelta, datetime
+
+# список адресов
+addresses = ["Россия, Ленинградская область, Санкт-Петербург, ул. Ленина, 10",
+             "Россия, Ленинградская область, Санкт-Петербург, ул. Советская, 20",
+             "Россия, Ленинградская область, Санкт-Петербург, ул. Желябова, 8",
+             "Россия, Калининградская область, Калининград, ул. Пролетарская, 10",
+             "Россия, Калининградская область, Балтийск, улица Красной Армии, 11",
+             "Швеция, Стокгольмская область, Стокгольм, ул. Гамла Стана, 10",
+             "Финляндия, Уусимаа, Хельсинки, ул. Эспланади, 1",
+             "Литва, Клайпедская область, Клайпеда, ул. Таурагес, 5",
+             "Латвия, Рига, ул. Андрейостас, 15",
+             "Эстония, Харьюмаа, Таллинн, ул. Вабадус, 7",
+             "Германия, Мекленбург-Передняя Померания, Росток, ул. Ленштрассе, 10",
+             "Польша, Поморское воеводство, Гданьск, ул. Тарнавска, 2",
+             "Литва, Клайпедская область, Паланга, ул. Базницию, 3"]
+
+# список id
+vehicleId = ["84350",
+             "12774",
+             "38618",
+             "42131"]
+
+
+# генерация времени остановок
+def generate_time():
+    # начало остановки
+    start_month = random.randint(1, 5)
+    start_day = random.randint(1, 28)
+    start_hour = random.randint(0, 23)
+    start_minute = random.randint(0, 59)
+    start_second = random.randint(0, 59)
+
+    start = datetime(2024, start_month, start_day, start_hour, start_minute, start_second)
+
+    # длительность остановки
+    duration_days = random.randint(0, 3)
+    duration_hours = random.randint(0, 1)
+    duration_minutes = random.randint(0, 59)
+    duration_seconds = random.randint(0, 59)
+
+    # конец остановки = начало + длительность
+    end = start + timedelta(days=duration_days, hours=duration_hours, minutes=duration_minutes, seconds=duration_seconds)
+
+    # формат времени
+    start_str = start.isoformat() + "Z"
+    end_str = end.isoformat() + "Z"
+
+    return start_str, end_str
+
+
+# генерация остановок
+def generate_stops():
+    stops = []
+    for _ in range(10):
+        start, end = generate_time()
+        stop = {
+            "address": random.choice(addresses),
+            "eventId": random.randint(1, 100),
+            "eventName": "string",
+            "start": start,
+            "end": end,
+            "duration": 0
+        }
+        stops.append(stop)
+    return stops
+
+
+# данные
+data = {
+    "vehicleId": random.choice(vehicleId),
+    "vehicleName": "Контейнер",
+    "receiverEmail": "demid.working@gmail.com",
+    "receiverPhone": "89110234562",
+
+    "stops": generate_stops()
+}
+
+# запись
+with open("data.json", "w") as file:
+    json.dump(data, file)
